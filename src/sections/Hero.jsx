68bb9566bfba2eec9f4a9a095f3f1aab02ab1770
StefanovicAleksa <aleksa.stefanovic0223@gmail.com>
2024-03-07 18:20:13 +0100

    updated hero section added get started for ss screens

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -42,6 +42,9 @@ export default function Hero() {
             <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient"></div>
             <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"></div>
         </div>
+        <div className="ss:hidden flex justify-center items-center">
+            <GetStarted />
+        </div>
     </section>
   )
 }
---
+++ src/sections/Stats.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Stats() {
+    return (
+        <section className='flex justify-between items-center flex-row flex-wrap sm:mb-20 mb-6'>
+
+        </section>
+    );
+}
